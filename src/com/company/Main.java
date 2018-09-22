034cc9fa676e7dc694105aa0b5adb8b05b16e35a
Brendon Dulam <brendondulam06@gmail.com>
2018-09-22 16:38:53 +0900

    Added key as user input validation of input Added testing if input is negative

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,30 +8,41 @@ import static java.lang.System.out;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        // Ask for String user input (message)
         out.println("Enter a message to encode or decode:");
 
-        String message;
-        try (Scanner scan = new Scanner(in)) {
-            message = scan.nextLine();
+        Scanner scan = new Scanner(in);
+        String message = scan.nextLine();
+
+        // Ask for Integer user input (key)
+        out.print("Enter a secret key (-26 to 26):");
+        int key = Integer.parseInt(scan.nextLine());
+
+        // Check if input is valid
+        while (key > 26 || key < -26) {
+            out.println("Invalid input.");
+            out.print("Enter a secret key (-26 to 26):");
+            key = Integer.parseInt(scan.nextLine());
         }
+
+
         StringBuilder out = new StringBuilder();
 
-        int messageLen;
-        messageLen = message.length();
+        int messageLen = message.length();
 
-        char key = 13;
         char curChar;
-
         for (int x = 0; x < messageLen; x++) {
             curChar = message.charAt(x);
 
             if (curChar >= 'A' && curChar <= 'Z') {
-                curChar += key;
+                curChar += (char) key;
                 if (curChar > 'Z') curChar -= 26;
+                else if (curChar < 'A') curChar += 26;
+
             } else if (curChar >= 'a' && curChar <= 'z') {
-                curChar += key;
+                curChar += (char) key;
                 if (curChar > 'z') curChar -= 26;
+                else if (curChar < 'a') curChar += 26;
             }
 
             out.append(curChar);
@@ -39,6 +50,6 @@ public class Main {
 
         System.out.println();
         System.out.println(out);
-
+        //
     }
 }
