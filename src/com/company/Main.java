a82e5e1bc6d429969110f92a7a3b302c7c7308db
Brendon Dulam <brendondulam06@gmail.com>
2018-09-22 14:39:52 +0900

    Adding characters and values

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,20 +2,28 @@ package com.company;
 
 import java.util.Scanner;
 
+import static java.lang.System.in;
+import static java.lang.System.out;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Enter a message to encode or decode:");
+        out.println("Enter a message to encode or decode:");
 
-        Scanner scan = new Scanner(System.in);
-        String message = scan.nextLine();
-
-        String out = "";
-
-        for (int x = message.length() - 1; x >= 0; x--) {
-            out += message.charAt(x);
+        String message;
+        try (Scanner scan = new Scanner(in)) {
+            message = scan.nextLine();
         }
+        StringBuilder out = new StringBuilder();
+
+        int messageLen;
+        messageLen = message.length();
+
+        char key = 13;
+
+        for (int x = 0; x < messageLen; x++)
+            out.append((char) (message.charAt(x) + 13));
 
         System.out.println();
         System.out.println(out);
