8646e1fb5a1ca8d2e03fd8636f2618163baeb916
Brendon Dulam <brendondulam06@gmail.com>
2018-09-22 14:10:54 +0900

    Asked for user input and output the reversed String

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World");
+        // write your code here
+        System.out.println("Enter a message to encode or decode:");
+
+        Scanner scan = new Scanner(System.in);
+        String message = scan.nextLine();
+
+        String out = "";
+
+        for (int x = message.length() - 1; x >= 0; x--) {
+            out += message.charAt(x);
+        }
+
+        System.out.println();
+        System.out.println(out);
+
     }
 }
